29e968b47dc7b0bd90e43f5b5e41f24aac060e7d
sravanigajula <119675534+qxf2intern@users.noreply.github.com>
2023-01-11 09:47:16 +0530

    practisedsleniumexercises

---
+++ 06_Form_Validation.py
@@ -0,0 +1,49 @@
+"""
+Check for the presence of absence of page elements
+
+DISCLAIMER: This code is aimed at Selenium BEGINNERS
+For more advanced tutorials and to learn how Qxf2 writes GUI automation, please visit our:
+a) Our GUI automation guides: http://qxf2.com/gui-automation-diy
+b) Other GitHub repos: https://github.com/qxf2
+
+AUTHOR: Avinash Shetty
+Contact: avinash@qxf2.com
+
+SCOPE:
+1) Launch Firefox driver
+2) Navigate to Qxf2 Tutorial page
+3) Find the Click me! button and click on it
+4) Check for the validation message
+5) Close the browser
+"""
+import time
+from selenium import webdriver
+
+# Create an instance of IE WebDriver
+driver = webdriver.Firefox()
+# Maximize the browser window
+driver.maximize_window()
+# Navigate to Qxf2 Tutorial page
+driver.get("https://profile.w3schools.com/log-in?redirect_url=https%3A%2F%2Fmy-learning.w3schools.com")
+
+# Find the click me! button and click it
+button  = driver.find_element("xpath" ,"//button[@class='Button_button__URNp+ Button_primary__d2Jt3 Button_fullwidth__0HLEu']")
+button.click()
+# Pause the script to wait for validation messages to load
+time.sleep(3)
+
+# KEY POINT: Check if the validation mesage for name field
+try:
+    driver.find_element("xpath", "//input[@id='current-password']")
+except Exception as e:
+    #This pattern of catching all exceptions is ok when you are starting out
+    result_flag = False
+else:
+    result_flag = True
+if result_flag is True:
+    print("Validation message for name present")
+else:
+    print("Validation message for name NOT present")
+
+# Close the browser window
+driver.close()
---
+++ 08_Hover.py
@@ -0,0 +1,54 @@
+"""
+Learn to hover over elements using Selenium
+
+DISCLAIMER: This code is aimed at Selenium BEGINNERS
+For more advanced tutorials and to learn how Qxf2 writes GUI automation, please visit our:
+a) Our GUI automation guides: http://qxf2.com/gui-automation-diy
+b) Other GitHub repos: https://github.com/qxf2
+
+AUTHOR: Avinash Shetty
+Contact: avinash@qxf2.com
+
+SCOPE:
+1) Launch Firefox driver
+2) Navigate to Qxf2 Tutorial page
+3) Click on Menu icon
+4) Hover over Resource and GUI automation and click on GUI automation link
+5) Close the browser
+"""
+
+import time
+from selenium import webdriver
+
+#Notice this extra import statement!
+from selenium.webdriver.common.action_chains import ActionChains
+
+# Create an instance of Firefox WebDriver
+driver = webdriver.Firefox()
+# Maximize the browser window
+driver.maximize_window()
+# Navigate to Qxf2 Tutorial page
+driver.get("https://www.amazon.in/")
+
+# Locate the Menu icon and click on it
+home = driver.find_element("xpath", "//i[@class='hm-icon nav-sprite']")
+home.click()
+
+# Locate the Resource element to hover over
+Trending = driver.find_element("xpath", "//div[normalize-space()='trending']")
+
+# KEY POINT: Use ActionChains to hover over elements
+action = ActionChains(driver)
+action.move_to_element(Trending)
+action.perform()
+time.sleep(2) #Adding waits to make the example more visual
+
+# Click the GUI automation link
+bestsellers = driver.find_element("xpath", "//ul[@class='hmenu hmenu-visible']//a[@class='hmenu-item'][normalize-space()='Best Sellers']")
+bestsellers.click()
+
+# Wait for 3 seconds for the page to load
+time.sleep(3)
+
+# Close the browser
+driver.close()
